0c048c3e0b764cca0f230b12a76541df87cf24b1
Furkan Karaketir <furkankaraketir2005@gmail.com>
2022-08-24 07:54:23 +0300

    Activity Added to See Studies

---
+++ app/src/main/java/com/kodgem/coachingapp/StudiesActivity.kt
@@ -0,0 +1,66 @@
+package com.kodgem.coachingapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.kodgem.coachingapp.adapter.StudiesRecyclerAdapter
+import com.kodgem.coachingapp.databinding.ActivityStudiesBinding
+import com.kodgem.coachingapp.models.Study
+
+class StudiesActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private lateinit var recyclerViewStudiesAdapter: StudiesRecyclerAdapter
+
+    private lateinit var recyclerViewStudies: RecyclerView
+    private var studyList = ArrayList<Study>()
+
+    private lateinit var binding: ActivityStudiesBinding
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStudiesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        db = Firebase.firestore
+        val layoutManager = GridLayoutManager(applicationContext, 2)
+        val intent = intent
+
+
+        val studentID = intent.getStringExtra("studentID")
+
+        recyclerViewStudies = binding.recyclerViewStudies
+        recyclerViewStudies.layoutManager = layoutManager
+        recyclerViewStudiesAdapter = StudiesRecyclerAdapter(studyList)
+        recyclerViewStudies.adapter = recyclerViewStudiesAdapter
+
+        if (studentID != null) {
+            db.collection("School").document("SchoolIDDDD").collection("Student")
+                .document(studentID).collection("Studies")
+                .orderBy("subjectTheme", Query.Direction.ASCENDING)
+                .addSnapshotListener { value, _ ->
+                    if (value != null) {
+                        studyList.clear()
+                        for (document in value) {
+                            val studyName = document.get("subjectTheme").toString()
+                            val sure = document.get("toplamCalisma").toString()
+                            val currentStudy = Study(studyName, sure)
+                            studyList.add(currentStudy)
+                        }
+                        recyclerViewStudiesAdapter.notifyDataSetChanged()
+                    }
+                }
+        }
+
+
+    }
+}
--- app/src/main/java/com/kodgem/coachingapp/adapter/StudiesRecyclerAdapter.kt
+++ app/src/main/java/com/kodgem/coachingapp/adapter/StudiesRecyclerAdapter.kt
@@ -7,23 +7,23 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.kodgem.coachingapp.R
+import com.kodgem.coachingapp.databinding.StudyGridRowBinding
 import com.kodgem.coachingapp.models.Study
-import com.kodgem.coachingapp.databinding.StudyRowBinding
 
 
 open class StudiesRecyclerAdapter(private val studyList: ArrayList<Study>) :
     RecyclerView.Adapter<StudiesRecyclerAdapter.StudyHolder>() {
 
     private lateinit var db: FirebaseFirestore
-    lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     class StudyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val binding = StudyRowBinding.bind(itemView)
+        val binding = StudyGridRowBinding.bind(itemView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudyHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.study_row, parent, false)
+        val view = inflater.inflate(R.layout.study_grid_row, parent, false)
         return StudyHolder(view)
     }
 
@@ -32,8 +32,8 @@ open class StudiesRecyclerAdapter(private val studyList: ArrayList<Study>) :
             db = FirebaseFirestore.getInstance()
             auth = FirebaseAuth.getInstance()
 
-            binding.studyNameTextView.text = studyList[position].studyName
-            binding.studyCountTextView.text = studyList[position].studyCount
+            binding.studyGridRowStudyNameTextView.text = studyList[position].studyName
+            binding.studyGridRowStudyMinuteTextView.text = studyList[position].studyCount
 
         }
     }
