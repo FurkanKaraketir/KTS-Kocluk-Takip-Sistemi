0baf08448e860c1a1f4b0c51709b9e350d775921
Furkan Karaketir <furkankaraketir2005@gmail.com>
2022-09-01 08:13:04 +0300

    Notification Style Update

--- app/src/main/java/com/kodgem/coachingapp/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/kodgem/coachingapp/services/MyFirebaseMessagingService.kt
@@ -10,9 +10,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
-import android.net.Uri
 import android.os.Build
-import android.os.Vibrator
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -32,19 +30,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         super.onMessageReceived(remoteMessage)
 
 
-// playing audio and vibration when user se reques
-        val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val r = RingtoneManager.getRingtone(applicationContext, notification)
-        r.play()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            r.isLooping = false
-        }
-
-        // vibration
-        val v = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        val pattern = longArrayOf(100, 300, 300, 300)
-        v.vibrate(pattern, -1)
-        val builder = NotificationCompat.Builder(this, "CHANNEL_ID")
+        val builder = NotificationCompat.Builder(this, "212121")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 //            builder.setSmallIcon(R.drawable.icontrans);
             builder.setSmallIcon(R.drawable.ic_baseline_book_24)
@@ -57,7 +43,10 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
         builder.setContentTitle(remoteMessage.notification!!.title)
         builder.setContentText(remoteMessage.notification!!.body)
+        builder.setVibrate(longArrayOf(300, 300))
+        builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
         builder.setContentIntent(pendingIntent)
+        builder.color = resources.getColor(R.color.purple_200)
         builder.setStyle(
             NotificationCompat.BigTextStyle().bigText(remoteMessage.notification!!.body)
         )
@@ -66,9 +55,9 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         mNotificationManager =
             applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = "Your_channel_id"
+            val channelId = "212121"
             val channel = NotificationChannel(
-                channelId, "Channel human readable title", NotificationManager.IMPORTANCE_HIGH
+                channelId, "Görevler Bildrimi", NotificationManager.IMPORTANCE_HIGH
             )
             mNotificationManager!!.createNotificationChannel(channel)
             builder.setChannelId(channelId)
